529a96efa77cc9fb42a7d42eb4610ff3bf7f0a82
DLManohar <damisettilakshmimanohar@yahoo.com>
2021-10-30 12:35:29 +0530

    removed a bug that made the weather app show data for a fixed location. And added more information to the main page  when requested for weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request')
 const forecast = (lat,lon,callback) => {
     coords = '' + lat + ',' + lon;
 
-    url = "http://api.weatherstack.com/current?access_key=7c135dc6e49947f7abf35b7749bb5410&query=40.7831,-73.9712" + coords
+    url = "http://api.weatherstack.com/current?access_key=7c135dc6e49947f7abf35b7749bb5410&query=" + coords
 
     request({url: url, json: true},(err,res) => {
         if(err) {
@@ -13,7 +13,9 @@ const forecast = (lat,lon,callback) => {
             callback('Unable to find location',undefined)
         }
         else {
-            callback(undefined,res.body.current.weather_descriptions[0])
+            // console.log(res.body)
+            const curr = res.body.current
+            callback(undefined,`Weather is ${curr.weather_descriptions[0]} with ${curr.humidity}% humidity. Temperature is ${curr.temperature}C, feels like ${curr.feelslike}C.`)
         }
     })
 }
